e7a33992e58e55bc764e1e90b6fe82fe669dbeb6
Inna <sraka.vurdalaka@mail.ru>
2024-02-13 15:17:06 +0700

    add modification into imdex.html, index.css, index.js

---
+++ js/index.js
@@ -0,0 +1,25 @@
+//3d scroll
+
+let Zspacing = -700;
+let lastPos = Zspacing / 5;
+let Frames = document.getElementsByClassName('frame');
+let frames = Array.from(Frames);
+let zVals = [];
+
+window.onscroll = function () {
+  let top = document.documentElement.scrollTop;
+  let delta = lastPos - top;
+
+  lastPos = top;
+
+  frames.forEach(function (nawElement, i) {
+    zVals.push((i * Zspacing) + Zspacing);
+    zVals[i] += delta * -5;
+    let frame = frames[i];
+    let transform = `translateZ(${zVals[i]}px)`;
+    let opacity = zVals[i] < Math.abs(Zspacing) ? 1 : 0;
+    frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+  });
+}
+
+window.scrollTo(0, 1)
